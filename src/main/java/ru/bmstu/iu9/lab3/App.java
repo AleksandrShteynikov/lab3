c54a175de72c8769a9ce4d9989064fda77149406
Snake5932 <shtjnk@yandex.ru>
2021-11-25 20:33:11 +0300

    Scripted auto-commit on change (2021-11-25 20:33:11) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab3/App.java
+++ src/main/java/ru/bmstu/iu9/lab3/App.java
@@ -1,9 +1,10 @@
 package ru.bmstu.iu9.lab3;
 
-import org.apache.spark.
+import org.apache.spark
 
 public class App {
     public static void main(String[] args) {
 
+        JavaRDD<String> airportsFile = sc.textFile();
     }
 }

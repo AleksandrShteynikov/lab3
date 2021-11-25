a8d6d65f9150ec71af09c0eaff56a828fa0095a4
Snake5932 <shtjnk@yandex.ru>
2021-11-25 21:48:05 +0300

    Scripted auto-commit on change (2021-11-25 21:48:05) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab3/App.java
+++ src/main/java/ru/bmstu/iu9/lab3/App.java
@@ -13,13 +13,18 @@ public class App {
     static final String FLIGHTS_FILE = "664600583_T_ONTIME_sample.csv";
     static final String AIRPORTS_REDUNDANT = "Code,Description";
     static final String FLIGHTS_REDUNDANT = "\"YEAR\",\"QUARTER\",\"MONTH\",\"DAY_OF_MONTH\",\"DAY_OF_WEEK\",\"FL_DATE\",\"UNIQUE_CARRIER\",\"AIRLINE_ID\",\"CARRIER\",\"TAIL_NUM\",\"FL_NUM\",\"ORIGIN_AIRPORT_ID\",\"ORIGIN_AIRPORT_SEQ_ID\",\"ORIGIN_CITY_MARKET_ID\",\"DEST_AIRPORT_ID\",\"WHEELS_ON\",\"ARR_TIME\",\"ARR_DELAY\",\"ARR_DELAY_NEW\",\"CANCELLED\",\"CANCELLATION_CODE\",\"AIR_TIME\",\"DISTANCE\",";
+    static final String SEPARATOR
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName(APP_NAME);
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String> airportsFile = sc.textFile(AIRPORTS_FILE);
         JavaRDD<String> pureAirports = airportsFile.filter(s -> !Objects.equals(s.trim(), AIRPORTS_REDUNDANT));
         JavaPairRDD<String, String> airports = pureAirports.mapToPair(s -> {
-
+            String[] airport = s.split(SEPARATOR, SPLIT_LIMIT);
+            String airportCode = airport[CODE_INDEX];
+            String airportName = airport[AIRPORT_INDEX];
+            airportCode = StringUtils.strip(airportCode, TRIMMER);
+            airportName = StringUtils.strip(airportName, TRIMMER);
         });
         JavaRDD<String> flightsFile = sc.textFile(FLIGHTS_FILE);
         JavaRDD<String> pureFlights = flightsFile.filter(s -> !Objects.equals(s.trim(), FLIGHTS_REDUNDANT));

d9e900b800a097da111172cdcd8a4e0847659383
Snake5932 <shtjnk@yandex.ru>
2021-11-25 21:33:29 +0300

    Scripted auto-commit on change (2021-11-25 21:33:29) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab3/App.java
+++ src/main/java/ru/bmstu/iu9/lab3/App.java
@@ -5,18 +5,18 @@ import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
+import java.util.Objects;
+
 public class App {
     static final String APP_NAME = "flights stats";
     static final String AIRPORTS_FILE = "L_AIRPORT_ID.csv";
     static final String FLIGHTS_FILE = "664600583_T_ONTIME_sample.csv";
+    static final String AIRPORTS_REDUNDANT = "Code,Description";
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName(APP_NAME);
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String> airportsFile = sc.textFile(AIRPORTS_FILE);
-        filtration = (String s) -> {
-            
-        }
-        JavaRDD<String> pureAirports = airportsFile.filter();
+        JavaRDD<String> pureAirports = airportsFile.filter(s -> !Objects.equals(s, AIRPORTS_REDUNDANT));
         JavaPairRDD<String, String> airports = pureAirports.mapToPair();
         JavaRDD<String> flightsFile = sc.textFile(FLIGHTS_FILE);
     }

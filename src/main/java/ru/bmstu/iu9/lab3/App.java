b8bc98c3b34fb348defe72de9fd1a40327fb31b3
Snake5932 <shtjnk@yandex.ru>
2021-11-25 20:37:38 +0300

    Scripted auto-commit on change (2021-11-25 20:37:38) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab3/App.java
+++ src/main/java/ru/bmstu/iu9/lab3/App.java
@@ -5,7 +5,8 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class App {
-    static final String APP_NAME = ""
+    static final String APP_NAME = "";
+    static final String 
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName(APP_NAME);
         JavaSparkContext sc = new JavaSparkContext(conf);

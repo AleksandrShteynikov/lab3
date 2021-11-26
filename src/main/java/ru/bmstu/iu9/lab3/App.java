aff9faad71a454243ae9a0157a50f7bd2cb53b29
AleksandrShteynikov <shtjnk91@gmail.com>
2021-11-26 09:43:29 +0300

    Scripted auto-commit on change (2021-11-26 09:43:29) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab3/App.java
+++ src/main/java/ru/bmstu/iu9/lab3/App.java
@@ -77,7 +77,8 @@ public class App {
         JavaPairRDD<Tuple2<String, String>, Flight> processedFlights = reducedFlights.map(reducedFlight -> {
             Flight processedFlight = reducedFlight._2;
             processedFlight.setLatePercent();
-            return new Tuple2<>(new Tuple2<>(airportsBroadcasted.value(reducedFlight._1._1)), processedFlight);
+            return new Tuple2<>(new Tuple2<>(airportsBroadcasted.value(reducedFlight._1._1),
+                                             airportsBroadcasted.value(reducedFlight._1._2)), processedFlight);
         });
         //processedFlights.saveAsTextFile("result");
     }

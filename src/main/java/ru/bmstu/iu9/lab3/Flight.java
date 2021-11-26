fa1a469bc1377c4974cff3ff6ba7aaf91655c3bb
AleksandrShteynikov <shtjnk91@gmail.com>
2021-11-26 07:25:22 +0300

    Scripted auto-commit on change (2021-11-26 07:25:22) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab3/Flight.java
+++ src/main/java/ru/bmstu/iu9/lab3/Flight.java
@@ -1,6 +1,8 @@
 package ru.bmstu.iu9.lab3;
 
-public class Flight {
+import java.io.Serializable;
+
+public class Flight implements Serializable {
     static int totalNum = 0;
     static int numOfLate = 0;
     static int numOfCancelled = 0;

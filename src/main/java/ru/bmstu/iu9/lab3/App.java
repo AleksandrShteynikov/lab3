86aefcf6045f6ad6988f624550760f97f63bb80f
AleksandrShteynikov <shtjnk91@gmail.com>
2021-11-26 08:14:32 +0300

    Scripted auto-commit on change (2021-11-26 08:14:32) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab3/App.java
+++ src/main/java/ru/bmstu/iu9/lab3/App.java
@@ -63,7 +63,7 @@ public class App {
             }
             return new Tuple2<>(new Tuple2<>(departureAirport, arrivalAirport), flight);
         });
-        JavaPairRDD<Tuple2<String, String>, Flight> reducedFlights = reduce
+        JavaPairRDD<Tuple2<String, String>, Flight> reducedFlights = reduceByKey
         //airports.saveAsTextFile("result");
     }
 }
